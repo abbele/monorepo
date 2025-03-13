c218394c4e75dc196ce87857c32691ea52e3e20b
Michele Abbrescia <michele.abbrescia@lotrek.it>
2025-03-13 14:27:06 +0100

    fix: update nuxt.config uikit importng core in modules

--- packages/uikit/nuxt.config.ts
+++ packages/uikit/nuxt.config.ts
@@ -2,8 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/scripts'],
-  extends: [
-    '@monorepo/core'
-  ]
+  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/scripts', '@monorepo/core'],
 })
